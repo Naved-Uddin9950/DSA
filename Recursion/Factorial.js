2d0bc5dc5d45ce75d207cdb3b841214ab0dc0a6f
Naved Uddin <naveduddin83@gmail.com>
2024-07-31 14:40:51 +0530

    factorial using recursion

--- Recursion/Factorial.js
+++ Recursion/Factorial.js
@@ -1,1 +1,10 @@
-const factorial = (number) => {}
+const factorial = (number) => {
+    if(number <= 1) {
+        return 1;
+    }
+    return factorial(number - 1) * number;
+}
+
+const number = 5;
+const result = factorial(number);
+console.log(result);
